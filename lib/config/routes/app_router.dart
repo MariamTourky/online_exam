9c9035adf06be71a6b1488c1f948b67788a33707
mariumtourky <amarium363@gmail.com>
2025-10-28 13:54:57 +0300

    SetUP Auth folders & Home Folder in Feauters Folder as Clean Arch

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,8 +1,8 @@
 import 'package:go_router/go_router.dart';
-//import 'package:online_exam/features/sign_up/presentation/views/sign_up_view.dart';
-// import '../../features/forget_password/presentation/views/forget_password_view.dart';
-// import '../../features/home/presentation/views/home_view.dart';
-// import '../../features/login/presentation/views/login_view.dart';
+import '../../features/forget_password/presentation/views/forget_password_view.dart';
+import '../../features/home/presentation/views/home_view.dart';
+import '../../features/login/presentation/views/login_view.dart';
+import '../../features/sign_up/presentation/views/sign_up_view.dart';
 import 'route_names.dart';
 class AppRouter {
   GoRouter get router => GoRouter(
@@ -12,22 +12,22 @@ class AppRouter {
       GoRoute(
         path: RouteNames.login,
         name: 'Login',
-    //    builder: (context, state) => const LoginView(),
+        builder: (context, state) => const LoginView(),
       ),
       GoRoute(
         path: RouteNames.signup,
         name: 'Signup',
-     //   builder: (context, state) => const SignUpView(),
+        builder: (context, state) => const SignUpView(),
       ),
       GoRoute(
         path: RouteNames.forgetPassword,
         name: 'ForgetPassword',
-        //builder: (context, state) => const ForgetPasswordView(),
+        builder: (context, state) => const ForgetPasswordView(),
       ),
       GoRoute(
         path: RouteNames.home,
         name: 'Home',
-       // builder: (context, state) => const HomeView(),
+       builder: (context, state) => const HomeView(),
       ),
     ],
   );
