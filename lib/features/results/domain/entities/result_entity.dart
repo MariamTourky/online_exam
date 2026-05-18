032f275925d4f2d5239b2a212f9448b764e97575
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2026-05-18 06:04:36 +0300

    fix: dart analyze & foramt

--- lib/features/results/domain/entities/result_entity.dart
+++ lib/features/results/domain/entities/result_entity.dart
@@ -18,6 +18,38 @@ class ResultEntity {
     this.createdAt,
     this.answeredQuestions,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'totalQuestions': totalQuestions,
+      'correctAnswers': correctAnswers,
+      'wrongAnswers': wrongAnswers,
+      'examTitle': examTitle,
+      'subjectName': subjectName,
+      'duration': duration,
+      'createdAt': createdAt?.toIso8601String(),
+      'answeredQuestions': answeredQuestions?.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  factory ResultEntity.fromJson(Map<String, dynamic> json) {
+    return ResultEntity(
+      totalQuestions: json['totalQuestions'] as int,
+      correctAnswers: json['correctAnswers'] as int,
+      wrongAnswers: json['wrongAnswers'] as int,
+      examTitle: json['examTitle'] as String,
+      subjectName: json['subjectName'] as String?,
+      duration: json['duration'] as int?,
+      createdAt: json['createdAt'] != null
+          ? DateTime.parse(json['createdAt'] as String)
+          : null,
+      answeredQuestions: (json['answeredQuestions'] as List<dynamic>?)
+          ?.map(
+            (e) => AnsweredQuestionEntity.fromJson(e as Map<String, dynamic>),
+          )
+          .toList(),
+    );
+  }
 }
 
 class AnsweredQuestionEntity {
@@ -32,6 +64,26 @@ class AnsweredQuestionEntity {
     this.selectedAnswerKey,
     this.correctAnswerKey,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'questionText': questionText,
+      'options': options.map((e) => e.toJson()).toList(),
+      'selectedAnswerKey': selectedAnswerKey,
+      'correctAnswerKey': correctAnswerKey,
+    };
+  }
+
+  factory AnsweredQuestionEntity.fromJson(Map<String, dynamic> json) {
+    return AnsweredQuestionEntity(
+      questionText: json['questionText'] as String,
+      options: (json['options'] as List<dynamic>)
+          .map((e) => AnswerOptionEntity.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      selectedAnswerKey: json['selectedAnswerKey'] as String?,
+      correctAnswerKey: json['correctAnswerKey'] as String?,
+    );
+  }
 }
 
 class AnswerOptionEntity {
@@ -39,4 +91,15 @@ class AnswerOptionEntity {
   final String? text;
 
   AnswerOptionEntity({this.key, this.text});
+
+  Map<String, dynamic> toJson() {
+    return {'key': key, 'text': text};
+  }
+
+  factory AnswerOptionEntity.fromJson(Map<String, dynamic> json) {
+    return AnswerOptionEntity(
+      key: json['key'] as String?,
+      text: json['text'] as String?,
+    );
+  }
 }
